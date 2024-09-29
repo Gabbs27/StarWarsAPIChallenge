06f6dea53e37297d792199be5f3fb5192456c73c
Gabriel <djabreu_@hotmail.com>
2024-09-29 13:15:54 -0400

    Fixed  Unit test

--- obj/Debug/net6.0/StarWarsAPIChallenge.AssemblyInfo.cs
+++ obj/Debug/net6.0/StarWarsAPIChallenge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StarWarsAPIChallenge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+149f5afdf6583ae900f34dada2b45606dc33aed5")]
 [assembly: System.Reflection.AssemblyProductAttribute("StarWarsAPIChallenge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StarWarsAPIChallenge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
